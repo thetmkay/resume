bdc3aa7d7d3bc73ba3d506d61165279db9223847
George Nishimura <thetmkay@gmail.com>
2014-10-22 01:27:29 +0100

    added design + first implementation. forgot to commit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express'),
   cons = require('consolidate'),
+  nunjucks = require('nunjucks'),
+  moment = require('moment'),
   path = require('path');
 
 var app = module.exports = express();
@@ -9,10 +11,17 @@ var app = module.exports = express();
  * Configuration
  */
 
+var env = new nunjucks.Environment(new nunjucks.FileSystemLoader('views'));
+env.addFilter('date', function(date, format) {
+  return moment(date).format(format);
+});
+env.express(app);
+
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('html', cons.nunjucks);
+
 app.set('view engine', 'html');
 // development only
 
