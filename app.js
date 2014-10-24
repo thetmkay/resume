29bea38a6c683f6cfc70a31e4d40ebb52b6e5e5a
George Nishimura <thetmkay@gmail.com>
2014-10-24 03:19:49 +0100

    fix mounting bug with views dir and nunjucks

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = module.exports = express();
  * Configuration
  */
 
-var env = new nunjucks.Environment(new nunjucks.FileSystemLoader('views'));
+var env = new nunjucks.Environment(new nunjucks.FileSystemLoader(path.join(__dirname,'views')));
 
 env.addFilter('date', function(date, format) {
   return moment(date).format(format);
@@ -22,13 +22,13 @@ env.express(app);
 
 // all environments
 app.set('port', process.env.PORT || 3000);
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('html', cons.nunjucks);
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 // development only
-
-
 
 /**
  * Routes
