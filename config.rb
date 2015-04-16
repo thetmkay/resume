caae641cfc011ab3a5d15ee4009d5070270caf31
George Nishimura <thetmkay@gmail.com>
2015-04-16 19:42:40 +0100

    adding import paths

--- config.rb
+++ config.rb
@@ -1,4 +1,3 @@
 sass_dir="src/scss"
 css_dir="public/css"
-add_import_path "node_modules/gn_components/scss"
-add_import_path "../gn_components/scss"
+additional_import_paths=["../gn_components/scss","node_modules/gn_components/scss"]
