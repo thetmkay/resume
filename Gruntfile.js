dcd03d5f7a7b33686337d6eef959b94b93efb896
George Nishimura <me@georgenishimura.com>
2015-07-15 03:07:31 +0100

    remove load-grunt-tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,8 @@
 module.exports = function(grunt) {
 
-	require('load-grunt-tasks')(grunt);
 
 	require('load-grunt-subtasks')(grunt,{
-			base:['./..']
+			base:['./..', './node_modules']
 	});
 
     grunt.initConfig({
